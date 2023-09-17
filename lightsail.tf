71d840f54b0ebe68ba56be6716d1aa8ebf2a081d
amapiano <nanfackzambounelie@gmail.com>
2023-09-17 14:54:32 -0500

    ticket number

---
+++ lightsail.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"  # Change to your desired AWS region
+}
+
+resource "aws_lightsail_instance" "example" {
+  name              = "example-instance"
+  availability_zone = "us-east-1a" 
+  blueprint_id      = "amazon_linux_2"
+  bundle_id         = "micro_2_0"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum install httpd -y
+              sudo systemctl start httpd
+              sudo systemctl enable httpd
+              echo "<h1>This Server is created using Terraform </h1>" | sudo tee /var/www/html/index.html
+              EOF
+}
+
+
+
